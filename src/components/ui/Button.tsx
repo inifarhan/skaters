2e0b04dd1cc3b935b2883911a0cfc37f5bdcc963
inifarhan <farhangunawan619@gmail.com>
2023-11-26 14:32:05 +0700

    update(button): update shadcn default button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -10,7 +10,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        // default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "before:ease relative overflow-hidden bg-primary text-white transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-1000 hover:before:-translate-x-40 duration-700",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
