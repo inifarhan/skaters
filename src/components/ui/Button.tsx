fab3b36039e25df2e3129f42b658e83a99e6113d
inifarhan <farhangunawan619@gmail.com>
2023-11-27 09:08:36 +0700

    update(button): change shadcn button color to green

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         // default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        default: "before:ease relative overflow-hidden bg-primary text-white transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-1000 hover:before:-translate-x-40 duration-700",
+        default: "before:ease relative overflow-hidden bg-emerald-600 text-white transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-1000 hover:before:-translate-x-40 duration-700",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
