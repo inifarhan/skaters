9d377a60c561d683564ad67ce8414b8f119b717d
inifarhan <farhangunawan619@gmail.com>
2023-11-27 09:12:25 +0700

    update(navbar): change next link tag to vanilla html anchor tag

--- src/components/layouts/ListItem.tsx
+++ src/components/layouts/ListItem.tsx
@@ -1,7 +1,7 @@
-import { cn } from '@/lib/utils';
-import Link from 'next/link';
-import React from 'react';
-import { NavigationMenuLink } from '../ui/NavigationMenu';
+import React from 'react'
+
+import { NavigationMenuLink } from '@/components/ui/NavigationMenu'
+import { cn } from '@/lib/utils'
 
 export const ListItem = React.forwardRef<
   React.ElementRef<'a'>,
@@ -10,16 +10,21 @@ export const ListItem = React.forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        {/* @ts-expect-error next link */}
-        <Link
+        <a
           ref={ref}
-          className={cn('block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground', className)}
-          {...props}>
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className,
+          )}
+          {...props}
+        >
           <div className='text-sm font-medium leading-none'>{title}</div>
-          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>{children}</p>
-        </Link>
+          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
+            {children}
+          </p>
+        </a>
       </NavigationMenuLink>
     </li>
-  );
-});
-ListItem.displayName = 'ListItem';
+  )
+})
+ListItem.displayName = 'ListItem'
