80e21d68bb39d4970170a7ff98f785f84278a9fe
inifarhan <farhangunawan619@gmail.com>
2023-11-27 14:38:37 +0700

    update(lobby): change catagories and products section heading

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,4 +1,5 @@
 import { Category } from '@prisma/client'
+import { ArrowRight } from 'lucide-react'
 
 import CategoryCard from '@/components/cards/CategoryCard'
 
@@ -28,13 +29,21 @@ const Categories = async () => {
       aria-labelledby='categories-heading'
       className='space-y-8 px-4 sm:px-6 lg:px-8 py-8 md:pt-10 lg:pt-24 sm:pb-28'
     >
-      <div className='mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center'>
-        <h2 className='text-3xl font-bold leading-[1.1] sm:text-3xl md:text-5xl'>
-          Categories
-        </h2>
-        <h3 className='max-w-[46rem] leading-normal text-muted-foreground sm:text-lg sm:leading-7'>
-          Find the best skateboarding gears from stores around the world
-        </h3>
+      <div className='flex items-end justify-between'>
+        <div className='flex flex-col space-y-4'>
+          <h2 className='max-w-sm text-3xl md:text-5xl text-start text-emerald-600 font-bold leading-[1.1]'>
+            Featured Categories
+          </h2>
+          <h3 className='leading-normal text-muted-foreground sm:text-lg sm:leading-7'>
+            Find the best skateboarding gears from stores around the world
+          </h3>
+        </div>
+        <a
+          href='/products'
+          className='hidden md:flex gap-1 text-emerald-700 hover:translate-x-1 hover:text-emerald-600 transition-all'
+        >
+          Shop the collection <ArrowRight />
+        </a>
       </div>
       <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
         {categories.map((category) => (
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,4 +1,4 @@
-import { ArrowRightIcon } from 'lucide-react'
+import { ArrowRight, ArrowRightIcon } from 'lucide-react'
 import Link from 'next/link'
 
 import ProductCard from '@/components/cards/ProductCard'
@@ -23,13 +23,21 @@ const Products = async () => {
       aria-labelledby='product-heading'
       className='space-y-8 px-4 sm:px-6 lg:px-8 py-8 md:pt-10 lg:pt-24'
     >
-      <div className='mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center'>
-        <h2 className='text-3xl font-bold leading-[1.1] sm:text-3xl md:text-5xl'>
-          Products
-        </h2>
-        <h3 className='max-w-[46rem] leading-normal text-muted-foreground sm:text-lg sm:leading-7'>
-          Explore products from around the world
-        </h3>
+      <div className='flex items-end justify-between'>
+        <div className='flex flex-col space-y-4'>
+          <h2 className='text-3xl md:text-5xl text-start text-emerald-600 font-bold leading-[1.1]'>
+            Popular Products
+          </h2>
+          <h3 className='leading-normal text-muted-foreground sm:text-lg sm:leading-7'>
+            Explore all products we offer from around the world
+          </h3>
+        </div>
+        <a
+          href='/products'
+          className='hidden md:flex gap-1 text-emerald-700 hover:translate-x-1 hover:text-emerald-600 transition-all'
+        >
+          Shop the collection <ArrowRight />
+        </a>
       </div>
       <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
         {products.map((product) => (
@@ -39,9 +47,8 @@ const Products = async () => {
       <Link
         href='/products'
         className={cn(
-          buttonVariants({
-            className: 'mx-auto flex w-fit',
-          }),
+          buttonVariants(),
+          'mx-auto flex w-fit hover:before:-translate-x-48',
         )}
       >
         View all products
