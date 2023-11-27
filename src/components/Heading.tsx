eae74dc2513c60fcbee67d6daf4a0d1b1b0d3e7d
inifarhan <farhangunawan619@gmail.com>
2023-11-27 14:41:34 +0700

    update(heading): change heading color to green

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -12,7 +12,7 @@ export const Heading: React.FC<HeadingProps> = ({
 }) => {
   return (
     <div {...props}>
-      <h2 className='text-3xl font-bold tracking-tight'>{title}</h2>
+      <h2 className='text-3xl text-emerald-600 font-bold tracking-tight'>{title}</h2>
       <p className='text-sm text-muted-foreground'>{description}</p>
     </div>
   )
