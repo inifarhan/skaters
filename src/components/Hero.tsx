f2b541104b56832992cd8023eb7d9f7716bcd238
inifarhan <farhangunawan619@gmail.com>
2023-11-27 14:36:30 +0700

    update(lobby): change hero green color

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -44,7 +44,7 @@ const Hero = async () => {
         viewBox='0 0 1440 320'
       >
         <path
-          fill='#34d399'
+          fill='#10b981'
           fill-opacity='1'
           d='M0,160L48,176C96,192,192,224,288,213.3C384,203,480,149,576,122.7C672,96,768,96,864,122.7C960,149,1056,203,1152,213.3C1248,224,1344,192,1392,176L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z'
         ></path>
@@ -70,7 +70,7 @@ const Hero = async () => {
           ) : null}
           <h1 className='font-bold tracking-tighter text-3xl sm:text-5xl lg:text-6xl max-w-sm sm:max-w-4xl'>
             An open source e-commerce project built by{' '}
-            <span className='text-emerald-500'>inifarhan</span>
+            <span className='text-emerald-600'>inifarhan</span>
           </h1>
           <h2 className='max-w-md sm:max-w-[42rem] leading-normal text-muted-foreground text-sm sm:text-xl sm:leading-8'>
             Buy and sell skateboarding gears from independent brands and stores
