54df209ff0e10f09e84839ad2d15b41eac55d856
inifarhan <farhangunawan619@gmail.com>
2023-11-28 08:51:24 +0700

    feat(dashboard): add products searching and pagination

--- src/app/(dashboard)/dashboard/stores/[storeId]/products/page.tsx
+++ src/app/(dashboard)/dashboard/stores/[storeId]/products/page.tsx
@@ -33,7 +33,7 @@ export default async function ProductsPage({
 
   return (
     <div>
-      <DataTable columns={columns} data={formattedData} />
+      <DataTable searchKey='name' columns={columns} data={formattedData} />
     </div>
   )
 }
--- src/components/ui/DataTable.tsx
+++ src/components/ui/DataTable.tsx
@@ -2,14 +2,20 @@
 
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from '@tanstack/react-table'
 import { PlusCircleIcon } from 'lucide-react'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
+import { useState } from 'react'
 
+import { Button, buttonVariants } from '@/components/ui/Button'
+import { Input } from '@/components/ui/Input'
 import {
   Table,
   TableBody,
@@ -19,22 +25,30 @@ import {
   TableRow,
 } from '@/components/ui/Table'
 import { cn } from '@/lib/utils'
-import { buttonVariants } from '@/components/ui/Button'
-import { Input } from '@/components/ui/Input'
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
+  searchKey: string
 }
 
 export function DataTable<TData, TValue>({
   columns,
   data,
+  searchKey,
 }: DataTableProps<TData, TValue>) {
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      columnFilters,
+    },
   })
 
   const params = useParams()
@@ -44,21 +58,24 @@ export function DataTable<TData, TValue>({
       <div className='flex items-center justify-between gap-2 px-1 pb-4'>
         <Input
           placeholder='Search'
-          // value={(table.getColumn(searchKey)?.getFilterValue() as string) ?? ''}
-          // onChange={(event) =>
-          //   table.getColumn(searchKey)?.setFilterValue(event.target.value)
-          // }
+          value={(table.getColumn(searchKey)?.getFilterValue() as string) ?? ''}
+          onChange={(event) =>
+            table.getColumn(searchKey)?.setFilterValue(event.target.value)
+          }
           className='max-w-sm'
         />
-        <Link aria-label='Create new row' href={`/dashboard/stores/${params.storeId}/products/new`}>
+        <Link
+          aria-label='Create new row'
+          href={`/dashboard/stores/${params.storeId}/products/new`}
+        >
           <div
             className={cn(
               buttonVariants({
-                variant: 'outline',
                 size: 'sm',
                 className: 'h-8',
               }),
-            )}>
+            )}
+          >
             <PlusCircleIcon className='mr-2 h-4 w-4' aria-hidden='true' />
             New
           </div>
@@ -89,7 +106,8 @@ export function DataTable<TData, TValue>({
               table.getRowModel().rows.map((row) => (
                 <TableRow
                   key={row.id}
-                  data-state={row.getIsSelected() && 'selected'}>
+                  data-state={row.getIsSelected() && 'selected'}
+                >
                   {row.getVisibleCells().map((cell) => (
                     <TableCell key={cell.id}>
                       {flexRender(
@@ -104,13 +122,32 @@ export function DataTable<TData, TValue>({
               <TableRow>
                 <TableCell
                   colSpan={columns.length}
-                  className='h-24 text-center'>
+                  className='h-24 text-center'
+                >
                   No results.
                 </TableCell>
               </TableRow>
             )}
           </TableBody>
         </Table>
+      </div>
+      <div className='flex items-center justify-end space-x-2 py-4'>
+        <Button
+          variant='outline'
+          size='sm'
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant='outline'
+          size='sm'
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
       </div>
     </>
   )
