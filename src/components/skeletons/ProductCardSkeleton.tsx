e4367f77e2319d5bc6d2d7bb642301947c8c3c07
inifarhan <farhangunawan619@gmail.com>
2023-11-29 10:47:26 +0700

    update(loading): update product card skeleton loading

--- src/components/skeletons/ProductCardSkeleton.tsx
+++ src/components/skeletons/ProductCardSkeleton.tsx
@@ -7,6 +7,7 @@ const ProductCardSkeleton = () => {
     <Card className='h-full overflow-hidden rounded-2xl'>
       <CardHeader className='p-0'>
         <AspectRatio ratio={1 / 1} className='m-3'>
+          <div className='absolute rounded-2xl inset-0 bg-gradient-to-t from-transparent to-zinc-950/50' />
           <Skeleton className='w-full h-full rounded-2xl' />
         </AspectRatio>
       </CardHeader>
