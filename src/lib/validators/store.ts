0ce0345d32477b288a8d0a1d4c202756d1368aea
inifarhan <farhangunawan619@gmail.com>
2023-12-07 07:47:24 +0700

    update(schema): update store zod schema

--- src/lib/validators/store.ts
+++ src/lib/validators/store.ts
@@ -1,8 +1,20 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const storeSchema = z.object({
-  name: z.string().min(3).max(50),
-  description: z.string().optional(),
+  name: z
+    .string()
+    .min(3, {
+      message: 'Name must contain at least 3 character(s)',
+    })
+    .max(50, {
+      message: 'Name must contain at most 50 character(s)',
+    }),
+  description: z
+    .string()
+    .max(500, {
+      message: 'Description must contain at most 500 character(s)',
+    })
+    .optional(),
 })
 
 export type storePayload = z.infer<typeof storeSchema>
